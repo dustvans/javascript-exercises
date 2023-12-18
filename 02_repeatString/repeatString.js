db680429f503fd325a013ffe878e86eb98885927
dustvans <dustinevans07@gmail.com>
2023-12-17 20:22:45 -0600

    Passed test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, numTimes) {
 
+    if (numTimes < 0) {
+        return "ERROR";
+    }
+
+    if (numTimes == 0) {
+        return '';
+    }
+
+    let result = string;
+    for (let index = 1; index < numTimes; index++) {
+        result = result + string;       
+    }
+
+    return result;
 };
 
 // Do not edit below this line
