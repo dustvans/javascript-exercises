78af06eab69f3a2bdf3e7db64460354a0b3a59e6
dustvans <dustinevans07@gmail.com>
2023-12-17 21:22:13 -0600

    Passed test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...Remove) {
+
+    for (const remove of Remove) {
+        for (let index = 0; index < array.length; index++)
+            if (array[index] === remove) {
+                array.splice(index, 1);
+            }
+    }
+
+    return array;
 
 };
 
