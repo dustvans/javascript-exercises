93f9cd133d8aee7d250d657aa120726dea0696a2
dustvans <dustinevans07@gmail.com>
2023-12-17 20:42:19 -0600

    Passed test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
 
+    for (let index = string.length - 1; index >= 0; index--) {
+
+        newString += string[index];
+        
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
