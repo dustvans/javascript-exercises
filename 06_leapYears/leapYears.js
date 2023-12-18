9903f497f0f209cd2f239400d3415e7750650752
dustvans <dustinevans07@gmail.com>
2023-12-18 17:10:34 -0600

    Test passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // divisible by 400
+    if (year % 400 == 0) {
+        return true
+    }
+    // divisible by 4 but not by 100
+    if (year % 4 == 0) {
+        if (year % 100 != 0) {
+            return true;
+        }
+    }
 
+    return false;
 };
 
 // Do not edit below this line
