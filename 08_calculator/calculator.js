7360bef9352d523db857cd0a3a3f770998aca1b2
dustvans <dustinevans07@gmail.com>
2023-12-23 19:06:57 -0600

    Finished test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((total, num) => {
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product, num) => {
+    return product * num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1;
+  }
+  let factorial = num;
+  for (let index = num; index > 1; index--) {
+    factorial = factorial * (index - 1);
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
