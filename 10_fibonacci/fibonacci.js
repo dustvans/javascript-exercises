54e938a7ca8eeb22092bc6ac633425c191779443
dustvans <dustinevans07@gmail.com>
2023-12-24 16:44:44 -0600

    Passed test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index == 0) {
+        return 0;
+    }
+
+    if (index < 0) {
+        return "OOPS"
+    }
+
+    let num = 1;
+    let fibArray = [1, 1, 2];
+    for (let i = 2; i < index; i++) {
+        num = (fibArray[i] + fibArray[i - 1]);
+        fibArray.push(num);
+    }
+
+    return fibArray[index - 1];
 
 };
 
