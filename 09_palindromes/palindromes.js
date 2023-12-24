19d9a625f8faacbcf0481da56cc8c7f7ae11065e
dustvans <dustinevans07@gmail.com>
2023-12-24 16:08:30 -0600

    Passed test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const oldString = string.toLowerCase().replace(/[\s,.!?]/g, "");
+
+    const splitString = oldString.split("");
+    const reverseString = splitString.slice().reverse();
+    const newString = reverseString.join("");
+
+    return oldString === newString;
 
 };
 
