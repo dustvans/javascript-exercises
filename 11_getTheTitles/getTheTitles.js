52995f6fd8a6e127fc0c47d47dd59901a7bea79d
dustvans <dustinevans07@gmail.com>
2023-12-27 08:48:29 -0600

    finished test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let bookTitles = [];
+    for (let index = 0; index < books.length; index++) {
+        const title = books[index].title;
+        bookTitles.push(title);
+    }
 
+    return bookTitles;
 };
 
 // Do not edit below this line
