a7c1cd2060db4ea9f8dfa6f506f6b9c74246086a
dustvans <dustinevans07@gmail.com>
2023-12-27 11:22:17 -0600

    Passed all test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let oldest = people[0];
+    if ('yearOfDeath' in oldest) {
+        oldest.age = oldest.yearOfDeath - oldest.yearOfBirth;
+    }
+    else {
+        oldest.age = (new Date().getFullYear()) - oldest.yearOfBirth;
+    }
 
+    for (let index = 0; index < people.length; index++) {
+        let temp = people[index];
+
+        if ('yearOfDeath' in temp) {
+            temp.age = temp.yearOfDeath - temp.yearOfBirth;
+        }
+        else {
+            temp.age = (new Date().getFullYear()) - temp.yearOfBirth;
+        }
+
+        if (oldest.age < temp.age) {
+            oldest = temp;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
